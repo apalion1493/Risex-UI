16126f1bb6d71339ad3b8b184d0adb17b176b6e8
apalion1493 <39518411+apalion1493@users.noreply.github.com>
2019-05-23 14:29:52 +0300

    правки 23.05

--- app/js/main.js
+++ app/js/main.js
@@ -1,32 +1,5 @@
-// $(document).ready(function(){
-//     $('.header-burger').click(function () {
-//         $(".right-menu").addClass('active');
-//         $(".milk-shadow").addClass('active');
-//     });
-//
-//     $('.right-menu__close').click(function () {
-//         $(".right-menu").removeClass('active');
-//         $(".milk-shadow").removeClass('active');
-//     });
-//
-//     $('.switch-btn').click(function(){
-//         $(this).toggleClass('switch-on');
-//     });
-// });
-//
-// jQuery(function($){
-//     $(document).mouseup(function (e){
-//         var div = $(".right-menu");
-//         if (!div.is(e.target) && div.has(e.target).length === 0) {
-//             $(".right-menu").removeClass('active');
-//             $(".milk-shadow").removeClass('active');
-//         }
-//     });
-// });
-
 let menuActive       = document.querySelector('.header-burger');
 let menuClose        = document.querySelector('.right-menu__close');
-// let switchButton     = document.querySelector('.switch-btn');
 
 menuActive.onclick = function() {
     document.querySelector('.right-menu').classList.toggle('active');
@@ -37,7 +10,3 @@ menuClose.onclick = function() {
     document.querySelector('.right-menu').classList.remove('active');
     document.querySelector('.milk-shadow').classList.remove('active');
 };
-
-// switchButton.onclick = function() {
-//     document.querySelector('.switch-btn').classList.toggle('switch-on');
-// };
