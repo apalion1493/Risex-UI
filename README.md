d5540b3973f63493ab9d6393b979dc046beba369
apalion1493 <39518411+apalion1493@users.noreply.github.com>
2019-06-04 13:42:14 +0300

    test 04.06

--- README.md
+++ README.md
@@ -28,3 +28,13 @@ https://apalion1493.github.io/Risex-UI/app/message.html
 https://apalion1493.github.io/Risex-UI/app/pro-deal.html
 
 https://apalion1493.github.io/Risex-UI/app/transaction-confirmation.html
+
+https://apalion1493.github.io/Risex-UI/app/sale-pro1.html
+
+https://apalion1493.github.io/Risex-UI/app/sale-pro2.html
+
+https://apalion1493.github.io/Risex-UI/app/sale-pro3.html
+
+https://apalion1493.github.io/Risex-UI/app/sales-setting.html
+
+https://apalion1493.github.io/Risex-UI/app/transaction-parameters.html
--- app/js/main.js
+++ app/js/main.js
@@ -1,44 +1,47 @@
-let menuActive       = document.querySelector('.header-burger');
-let menuClose        = document.querySelector('.right-menu__close');
+document.addEventListener('DOMContentLoaded', function () {
+    let menuActive       = document.querySelector('.header-burger');
+    let menuClose        = document.querySelector('.right-menu__close');
 
-menuActive.onclick = function() {
-    document.querySelector('.right-menu').classList.toggle('active');
-    document.querySelector('.milk-shadow').classList.toggle('active');
-};
+    menuActive.onclick = function() {
+        document.querySelector('.right-menu').classList.toggle('active');
+        document.querySelector('.milk-shadow').classList.toggle('active');
+    };
 
-menuClose.onclick = function() {
-    document.querySelector('.right-menu').classList.remove('active');
-    document.querySelector('.milk-shadow').classList.remove('active');
-};
+    menuClose.onclick = function() {
+        document.querySelector('.right-menu').classList.remove('active');
+        document.querySelector('.milk-shadow').classList.remove('active');
+    };
 
 
-// var elem = document.getElementById("textarea-text");
-// // elem.innerHTML = "sadasd";
-//
-//  elem.onblur = function() {
-//      if (elem.innerHTML === "") {
-//          elem.innerHTML = "Напишите сообщение...";
-//      }
-// };
-//
-// elem.onfocus = function() {
-//     if (elem.innerHTML === "" || elem.innerHTML === "Напишите сообщение...") {
-//         elem.innerHTML = "";
-//     }
-//
-// };
 
-//
-// function select(element) {
-//     var value = element.getAttribute("data-value"); // Считываем значение выбранного элемента
-//     var nodes = element.parentNode.childNodes; // Получаем все остальные элементы
-//     for (var i = 0; i < nodes.length; i++) {
-//         /* Отфильтровываем посторонние элементы text и input */
-//         if (nodes[i] instanceof HTMLParagraphElement) {
-//             /* Добавляем active у выбранного элемента, стирая данный класс у всех остальных */
-//             if (value == nodes[i].getAttribute("data-value")) nodes[i].className = "active";
-//             else nodes[i].className = "";
-//         }
-//     }
-//     document.getElementById("my_select").value = value; // Устанавливаем в hidden-поле выбранное значение
-// }
+    document.addEventListener('click', function (event) {
+
+        if (!event.target.classList.contains('accordion-toggle')) return;
+
+        var content = document.querySelector(event.target.hash);
+        if (!content) return;
+
+        event.preventDefault();
+
+        if (content.classList.contains('active')) {
+            content.classList.remove('active');
+            return;
+        }
+
+        var accordions = document.querySelectorAll('.accordion-content.active');
+        for (var i = 0; i < accordions.length; i++) {
+            accordions[i].classList.remove('active');
+        }
+
+        content.classList.toggle('active');
+
+    });
+
+    let el = document.querySelector('.accordion-toggle');
+    el.onclick = function() {
+        el.classList.toggle('active');
+    };
+});
+
+
+
